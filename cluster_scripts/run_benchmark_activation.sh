c13e5d07834c19d98e0202724a16facc4049bf62
Quangle07 <2007huyquang@gmail.com>
2026-08-05 16:54:45 +0100

    Add benchmark activation script for job submission

---
+++ cluster_scripts/run_benchmark_activation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#$ -N ezkl_activation
+#$ -cwd
+#$ -l h_rt=24:00:00
+#$ -l h_vmem=64G
+#$ -V
+
+module load anaconda
+source activate /exports/eddie/scratch/$USER/ezkl_env
+
+python benchmark_activation_complete.py
